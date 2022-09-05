751ea02b001f42bcd663a26fc3ad8e1f2466d0c4
alok-clayfin <110982204+alok-clayfin@users.noreply.github.com>
2022-09-06 00:33:28 +0530

    1 > 1.1

---
+++ Dog.java
@@ -0,0 +1,26 @@
+
+public class Dog {
+	// Class Declaration
+	    // Instance Variables
+	    String breed;
+	    String size;
+	    int age;
+	    String color;
+
+	  
+	    // method 1
+	    public String getInfo() {
+	        return ("Breed is: "+breed+" Size is:"+size+" Age is:"+age+" color is: "+color);
+	    }
+	    
+
+	    public static void main(String[] args) {
+	        Dog maltese = new Dog();
+	        maltese.breed="Maltese";
+	        maltese.size="Small";
+	        maltese.age=2;
+	        maltese.color="white";
+	        System.out.println(maltese.getInfo());
+	    }
+}
+
